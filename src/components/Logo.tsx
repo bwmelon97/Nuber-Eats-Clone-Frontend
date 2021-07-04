60695b965176076ab35a0f8b26172781052fb1c1
Soogeun Park <bwmelon97@naver.com>
2021-07-04 06:03:35 +0000

    Logo: modify Props to optional

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LOGO_IMG_URL } from "@constants";
 
 type LogoProps = {
-    className: string;
+    className?: string;
 }
 const Logo = ({className}: LogoProps) => <img src={LOGO_IMG_URL} alt='logo' className={className} />
 
