dce335537177e1e1435b9d262c26787b6ce69f1a
Soogeun Park <bwmelon97@naver.com>
2021-07-07 07:14:35 +0000

    MainPage - CategoryListBox: fix warning with key props in array

--- src/pages/clients/MainPage/CategoryListBox.tsx
+++ src/pages/clients/MainPage/CategoryListBox.tsx
@@ -10,8 +10,8 @@ export const CategoryListBox = () => {
         <div className='px-5 mt-5 max-w-5xl mx-auto overflow-x-auto' >
             <div className='mb-3 flex justify-evenly min-w-max' >
                 {data?.getAllCategories.categories?.map( (category, idx) => (
-                    <div className='flex flex-col items-center rounded-2xl cursor-pointer group' >
-                        <div key={idx} 
+                    <div key={idx} className='flex flex-col items-center rounded-2xl cursor-pointer group' >
+                        <div 
                             className='h-16 w-16 rounded-full mx-2 bg-cover group-hover:bg-gray-200' 
                             style={{backgroundImage:`url(${category.coverImg})`}}
                         />
