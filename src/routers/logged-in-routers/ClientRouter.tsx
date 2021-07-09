6b470517b6603b47e105aed468a537ae45011875
Soogeun Park <bwmelon97@naver.com>
2021-07-09 14:05:40 +0000

    SearchPage: create Search Page & add to client router

--- src/routers/logged-in-routers/ClientRouter.tsx
+++ src/routers/logged-in-routers/ClientRouter.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Route } from 'react-router'
-import { MainPage } from "@pages/clients";
+import { MainPage, SearchPage } from "@pages/clients";
 
 const ClientRouter = [
-    <Route exact path='/' > <MainPage /> </Route>
+    <Route key={1} exact path='/' > <MainPage /> </Route>,
+    <Route key={2} path='/search' > <SearchPage /> </Route>
 ]
 
 export default ClientRouter
