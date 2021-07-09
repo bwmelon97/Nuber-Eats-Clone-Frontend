e5726f748e76f616e9ba270987ee04293b03af93
Soogeun Park <bwmelon97@naver.com>
2021-07-09 14:51:04 +0000

    GlobalStyle: 쓰레기 같은 antd a태그 스타일을 제거함

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
-import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import tw, { GlobalStyles as BaseStyles } from 'twin.macro'
 
 const CustomStyles = createGlobalStyle`
   body {
-    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
     ${tw`antialiased`}
+  }
+
+  * {
+    --antd-wave-shadow-color: rgba(255,255,255,0);
+  }
+  a {
+    color: inherit;
+  }
+  a:hover {
+    color: inherit;
   }
 `
 
