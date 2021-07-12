63bbeb5d895c28947f6297e6d867fc9ee67155f0
Soogeun Park <bwmelon97@naver.com>
2021-07-12 13:45:28 +0000

    NotFoundPage: change home button to go back

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,24 +4,25 @@ import { useHistory } from 'react-router';
 
 function NotFoundPage () {
     const history = useHistory();
-    const onClick = () => history.push('/')
+    const goHome = () => history.push('/')
+    const goBack = () => history.goBack();
 
     return (
         <div className='absolute top-0 bottom-0 left-0 right-0 flex justify-center' >
             <div className='max-w-md w-full py-40 px-4 flex flex-col items-center' >
                 {/* 언젠가 이미지 키우고 만다 ㅡ.ㅡ */}
                 <img 
-                    onClick={onClick}
+                    onClick={goHome}
                     src={_404_IMG} alt='404_img' 
                     className='mb-6 cursor-pointer' 
                 />
                 <h1 className='text-3xl font-semibold'> Nothing to Eat Here... </h1>
                 <h3 className='mt-3' > Let’s discover something delicious. </h3>
                 <button 
-                    onClick={onClick}
+                    onClick={goBack}
                     className='mt-6 bg-black text-white w-full py-3 font-semibold text-lg hover:opacity-80' 
                 > 
-                    Find Food 
+                    Go Back
                 </button>
             </div>
         </div>
