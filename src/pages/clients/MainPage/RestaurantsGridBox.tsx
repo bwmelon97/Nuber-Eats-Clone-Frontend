1f51a83a886fec27778c6bec9bb204b419c21e6f
Soogeun Park <bwmelon97@naver.com>
2021-07-12 14:46:47 +0000

    RestaurantsGridBox: restaurant 데이터 없는 경우 handleing

--- src/pages/clients/MainPage/RestaurantsGridBox.tsx
+++ src/pages/clients/MainPage/RestaurantsGridBox.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import PaddingXRule from "@components/clients/PaddingXDiv";
 import { OverWriteClassNameProps } from "@components/OverWriteClassNameProps";
 import { RestaurantFragment } from "@gql-types/RestaurantFragment";
+import { _404_IMG } from "@constants";
 
 type RestaurantGridBoxProps = OverWriteClassNameProps & {
     restaurants: RestaurantFragment[] | null;
@@ -10,11 +11,16 @@ type RestaurantGridBoxProps = OverWriteClassNameProps & {
 
 export const RestaurantsGridBox: React.FC<RestaurantGridBoxProps> = ({ className, restaurants }) => {
 
+    const history = useHistory();
+    const goHome = () => history.push('/')
+
     return (
         <PaddingXRule className='mt-3 pb-32' >
             <div
                 className={`${className && className} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-x-6 gap-y-10 `}
-            >{ restaurants?.map( (restaurant, idx) => 
+            >{ restaurants && restaurants.length > 0 ?
+
+                restaurants.map( (restaurant, idx) => 
                 <div key={idx}>
                     <Link to={`/restaurant/${restaurant.id}`}>
                         <div className='h-64 bg-cover bg-center mb-4' style={{backgroundImage: `url(${restaurant.coverImg})`}} >
@@ -37,8 +43,27 @@ export const RestaurantsGridBox: React.FC<RestaurantGridBoxProps> = ({ className
                             {restaurant.address} 
                         </span> 
                     </div>
+                </div>) :
+
+                <div className='absolute top-0 bottom-0 left-0 right-0 flex justify-center' >
+                    <div className='max-w-md w-full pt-60 px-4 flex flex-col items-center' >
+                        {/* 언젠가 이미지 키우고 만다 ㅡ.ㅡ */}
+                        <img 
+                            onClick={goHome}
+                            src={_404_IMG} alt='404_img'
+                            className='mb-6 cursor-pointer' 
+                        />
+                        <h1 className='text-3xl font-semibold'> 찾으시는 결과물이 없어요 😭 </h1>
+                        <h3 className='mt-3' > 다른 먹거리를 찾아보는 건 어떨까요? </h3>
+                        <button 
+                            onClick={goHome}
+                            className='mt-6 bg-black text-white w-full py-3 font-semibold text-lg hover:opacity-80' 
+                        > 
+                            Find Food
+                        </button>
+                    </div>
                 </div>
-            )}</div>
+        }</div>
         </PaddingXRule>
     )
 }
